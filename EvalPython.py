4090b4fa187e0dc138ea110bc25d327de04fb747
ClaireEsnault <c.esnault@rns.ecolescreatives.com>
2020-12-09 11:41:19 +0100

    modif

---
+++ EvalPython.py
@@ -0,0 +1,13 @@
+from colorama import init
+init()
+from colorama import Fore, Back, Style
+
+
+from random import*
+bibliotheque = ['BLONDE', 'CALMER', 'VAPEUR', 'DINGUE', 'ZOMBIE', 'VALISE', 'VIOLET', 'THUNES', 'TAQUIN', 'STYLOS']
+mot = ['.', '.', '.', '.', '.', '.']
+
+motChoisit=choice(bibliotheque)
+
+print("le mot choisit est", motChoisit)
+input ()
